d455ca720c3f10a3085655942d5913c25e7a1ffe
Ahmad Mahmood <23100146@lums.edu.pk>
2021-11-17 15:48:29 +0500

    images masla solved

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flaskext.mysql import MySQL
 import mysql.connector
-import configparser
+
+from werkzeug.utils import send_file
 
 
 app = Flask(__name__)
@@ -53,7 +54,9 @@ def signin():
     else:
         return render_template('signin.html')
 
-    
-
+@app.route('/res/<name>', methods = ["GET"])
+def fetchImg(name):
+    print('here')
+    return send_from_directory('Resources', name)
 if __name__ == '__main__':
     app.run(debug=True)
