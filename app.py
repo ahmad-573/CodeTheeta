f41fc9cb44685f303b6a39ae225d725787fb43fe
Fahad-Touseef <23100032@lums.edu.pk>
2021-11-27 17:12:46 +0500

    token

--- app.py
+++ app.py
@@ -18,7 +18,9 @@ app.config['MYSQL_DB'] = 'ct_db'
 db = mysql.connector.connect(
     user='project_user', database='ct_db', password='password123')
 
-# USE CASES 
+# USE CASES
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -67,12 +69,15 @@ def signin():
 
     return render_template('signin.html')
 
+
 @app.route('/view_problem/<int:id>')
 def view_problem(id):
     cur = db.cursor(buffered=True)
-    cur.execute("select problem_id,difficulty,statement from problem_set where problem_id = '" + str(id) + "'")
+    cur.execute(
+        "select problem_id,difficulty,statement from problem_set where problem_id = '" + str(id) + "'")
     result = cur.fetchall()
-    return render_template('view_problem.html',result=result)
+    return render_template('view_problem.html', result=result)
+
 
 @app.route('/delete_problem/<int:id>')
 def delete_problem(id):
@@ -83,17 +88,18 @@ def delete_problem(id):
 
 
 # DASHBOARDS
-@app.route('/dashboard-user.html/', methods=['POST','GET'])
+@app.route('/dashboard-user.html/', methods=['POST', 'GET'])
 def dash_user():
     if request.method == "POST":
         content = request.get_json()
         cur = db.cursor(buffered=True)
 
     cur = db.cursor(buffered=True)
-    cur.execute("select problem_id, difficulty, times_solved, statement from problem_set")
+    cur.execute(
+        "select problem_id, difficulty, times_solved, statement from problem_set")
     result = cur.fetchall()
     print(result)
-    return render_template('dashboard-user.html',result=result)
+    return render_template('dashboard-user.html', result=result)
 
 
 @app.route('/dashboard-admin.html/')
@@ -101,13 +107,25 @@ def dash_admin():
     if request.method == "POST":
         content = request.get_json()
         cur = db.cursor(buffered=True)
+        try:
+            cur.execute('insert into problem_set(difficulty,statement, test_case1, test_case2, output1, output2) values(%s,%s,%s, %s,%s,%s)',
+                        (content['difficulty'], content['statement'], content['tc1'], content['tc2'], content['o1'], content['o2']))
+
+            db.commit()
+            success = 'Yes'
+        except:
+            success = 'No'
+        return jsonify({'success': success})
 
     cur = db.cursor(buffered=True)
-    cur.execute("select problem_id, difficulty, times_solved, statement from problem_set")
+    cur.execute(
+        "select problem_id, difficulty, times_solved, statement from problem_set")
     result = cur.fetchall()
-    return render_template('dashboard-admin.html',result=result)
+    return render_template('dashboard-admin.html', result=result)
 
 # IMAGE RENDERING
+
+
 @app.route('/res/<name>', methods=["GET"])
 def fetchImg(name):
     return send_from_directory('Resources', name)
