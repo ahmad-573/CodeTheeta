5a13b2def4116f7bcce1393ef13d90e7b9bfdd35
Ahmad Mahmood <23100146@lums.edu.pk>
2021-12-14 22:59:07 +0500

    populate db

---
+++ add_data/script_problems.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import pandas as pd
+import hashlib
+from random import randint
+import random
+
+def get_shuffled_substr(s):
+    n = len(s)
+    start = randint(0,n-1)
+    end = randint(start+1,n)
+    l = list(s[start:end])
+    random.shuffle(l)
+    return ''.join(l)
+
+    
+
+
+def run_script():
+    db = mysql.connector.connect(
+        user='project_user', database='ct_db', password='password123')
+    og_title = 'Thisisarandomtitlegenerator'
+    lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non imperdiet sem, ac malesuada mi. Mauris sed nunc nisi. Duis vitae semper purus. Aliquam dolor enim, aliquam id urna sed, dignissim tincidunt ipsum. Proin fringilla mauris bibendum augue venenatis laoreet sit amet id nisl. Mauris feugiat nisi tristique ante facilisis, sed auctor nisi pulvinar. Nam in velit porttitor, accumsan felis ut, imperdiet neque. Praesent ac fringilla ex. Fusce volutpat tellus vitae tellus scelerisque faucibus. Vivamus commodo molestie nisl non facilisis. Donec sollicitudin velit felis, non malesuada ligula vestibulum eu.'
+    cur = db.cursor(buffered=True)
+    for i in range(5000):
+        title = get_shuffled_substr(og_title)
+        difficulty = randint(1,3)
+        cur.execute("insert into problem_set(title,difficulty,statement,test_case1,test_case2,output1,output2) values(%s,%s,%s,%s,%s,%s,%s)",(title,difficulty,lorem,lorem,lorem,lorem,lorem))
+        db.commit()
+        cur.execute("select max(problem_id) from problem_set")
+        r = cur.fetchall()[0][0]
+        r = int(r)
+        cur.execute("insert into created values(%s,%s)",(r,1))
+        db.commit()
+
+
+    
+
+if __name__ == '__main__':
+    run_script()
---
+++ add_data/script_solver.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import pandas as pd
+import hashlib
+
+def run_script():
+    db = mysql.connector.connect(
+        user='project_user', database='ct_db', password='password123')
+
+    un = pd.read_csv('add_data/usernames.txt')
+    pw = pd.read_csv('add_data/passwords.txt')
+    fn = pd.read_csv('add_data/fullnames.txt')
+    un = un.iloc[0:5000]
+    fn = fn.iloc[0:5000]
+    uc = 'otlak33'
+    fc = 'Aaren'
+    pc = 'V76szXAoQ9'
+    print(type(un.iloc[0][uc]))
+    cur = db.cursor(buffered=True)
+    for i in range(un.shape[0]):
+        p = hashlib.sha256(str(pw.iloc[i][pc]).encode()).hexdigest()
+        cur.execute('insert into solver(full_name,username,password) values(%s,%s,%s)', (fn.iloc[i][fc],un.iloc[i][uc],p))
+        db.commit()
+
+if __name__ == '__main__':
+    run_script()
